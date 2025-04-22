5b9a9ad509bbc0352407be6669036eb7e119264e
consistentlyUnfunny <148969889+consistentlyUnfunny@users.noreply.github.com>
2025-04-22 16:13:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+st.set_option('server.showErrorDetails', True)
+
 # Page configuration
 st.set_page_config(page_title="Stock Volatility Prediction Dashboard", layout="wide")
 
